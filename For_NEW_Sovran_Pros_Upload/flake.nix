aeeceae9802bb33cd786faef1afcd1fdf29bb2e9
naturallaw77 <advbuwxeew@use.startmail.com>
2023-06-01 09:56:06 -0700

    updated local Sovran Pro Flake

--- For_NEW_Sovran_Pros_Upload/flake.nix
+++ For_NEW_Sovran_Pros_Upload/flake.nix
@@ -1,14 +1,22 @@
 {
-	description = "The Ultimate Sovran Pro Configuration from Sovran Systems";
+	description = "Sovran_SystemsOS for the Sovran Pro from Sovran Systems";
 
 	inputs = {
-		Sovran_Systems.url = "git+https://git.sovransystems.com/Sovran_Systems/Sovran_Pro";
+		
+		Sovran_Systems.url = "git+https://git.sovransystems.com/Sovran_Systems/Sovran_SystemsOS";
+	
 	};
 
 	outputs = { self, Sovran_Systems, ... }@inputs: {
+		
 		nixosConfigurations."nixos" = Sovran_Systems.inputs.nixpkgs.lib.nixosSystem {
+			
 			system = "x86_64-linux";
- 			modules = [ Sovran_Systems.nixosModules.Sovran_Pro ];
+ 			
+ 			modules = [ Sovran_Systems.nixosModules.Sovran_SystemsOS];
+		
 		};
+	
 	};
+
 }
