20327720d16bf1f4a3b336d1148d77e711064029
naturallaw77 <advbuwxeew@use.startmail.com>
2023-06-13 12:09:54 -0700

    updated onlyoffice and nginx ssl

--- modules/onlyoffice.nix
+++ modules/onlyoffice.nix
@@ -12,4 +12,17 @@ let
 	services.nginx.defaultSSLListenPort = 9443;
 	services.nginx.defaultHTTPListenPort = 9080;
 
+	security.acme = {
+	  acceptTerms = true;
+	  defaults.email = "cert+${caddy_email_for_zerossl}";
+	  certs."${personalization.onlyoffice_url}" = {
+	    webroot = "/var/lib/acme/challenges-com";
+	    email = "cert+${caddy_email_for_zerossl}";
+	    group = "nginx";
+	    extraDomainNames = [ "www.${personalization.onlyoffice_url}" ];
+		};
+	};
+	
+	users.users.nginx.extraGroups = [ "acme" ];
+
 }
