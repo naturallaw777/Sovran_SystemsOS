21293ebb25f069f82261906695b6d4c8523d84e0
naturallaw77 <advbuwxeew@use.startmail.com>
2023-06-13 14:04:11 -0700

    trouble shooting onlyoffice webserver

--- modules/onlyoffice.nix
+++ modules/onlyoffice.nix
@@ -6,22 +6,4 @@ let
 {
 	services.onlyoffice = {
 		enable = true;
-		hostname = "${personalization.onlyoffice_url}";
-	};
-
-	services.nginx.defaultSSLListenPort = 9443;
-	services.nginx.defaultHTTPListenPort = 9080;
-
-	security.acme = {
-	  acceptTerms = true;
-	  defaults.email = "cert+${personalization.caddy_email_for_zerossl}";
-	  certs."${personalization.onlyoffice_url}" = {
-	    webroot = "/var/lib/acme/challenges-com";
-	    email = "cert+${personalization.caddy_email_for_zerossl}";
-	    group = "nginx";
-		};
-	};
-	
-	users.users.nginx.extraGroups = [ "acme" ];
-
 }
