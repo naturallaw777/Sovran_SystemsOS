5a16f5172db403e5a1e67b4ed508d9f10630d91a
naturallaw77 <advbuwxeew@use.startmail.com>
2023-06-21 20:58:17 -0700

    added sturcture for updates

--- file_fixes_and_new_services/test.sh
+++ file_fixes_and_new_services/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
    
-(date && test.sh) >> /var/lib/beacons/awesome.log
-
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1> /var/lib/beacons/awesome.log 2>&1
 
 #### CHECK TO SEE IF IT HAS BEEN RUN BEFORE ####
