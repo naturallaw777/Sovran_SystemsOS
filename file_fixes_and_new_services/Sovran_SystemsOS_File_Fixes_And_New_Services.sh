5f8572b5e12d840cf818b7790a322b66f13b6cae
naturallaw77 <advbuwxeew@use.startmail.com>
2023-06-21 21:19:29 -0700

    updated structure for updates

--- file_fixes_and_new_services/Sovran_SystemsOS_File_Fixes_And_New_Services.sh
+++ file_fixes_and_new_services/Sovran_SystemsOS_File_Fixes_And_New_Services.sh
@@ -2,10 +2,23 @@
 
 cd /home/free/Downloads
 
+
+
+#### TEST SCRIPT 1 ####
+
 /run/current-system/sw/bin/wget "https://git.sovransystems.com/Sovran_Systems/Sovran_SystemsOS/raw/branch/main/file_fixes_and_new_services/test.sh"
 
 /run/current-system/sw/bin/bash /home/free/Downloads/test.sh
 
 rm -rf /home/free/Downloads/test.sh
+
+
+
+#### TEST SCRIPT 2 ####
+
+
+
+
+#### REMOVAL OF MAIN SCRIPT ####
 
 rm -rf /home/free/Downloads/Sovran_SystemsOS_File_Fixes_And_New_Services.sh
