9931578960ba0541d029bfe8bc603af1ec8bf88d
naturallaw77 <advbuwxeew@use.startmail.com>
2023-07-18 12:01:47 -0700

    added nodejs-slim-16.20.1 as nixbitcoin needs it

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -171,7 +171,7 @@ in
 
 	nixpkgs.config.permittedInsecurePackages = [
 	               "nodejs-16.20.1"
-	               "nodejs-slim-16.20.0"
+	               "nodejs-slim-16.20.1"
 	             ];
 
 
