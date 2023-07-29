3bbcef1c5b41e09a2f8640c4ce00cd185e85e575
naturallaw77 <advbuwxeew@use.startmail.com>
2023-07-28 19:43:21 -0700

    udpated Sovran Pro flake.nix

--- for_new_sovran_pros/flake.nix
+++ for_new_sovran_pros/flake.nix
@@ -13,7 +13,13 @@
 			
 			system = "x86_64-linux";
  			
- 			modules = [ Sovran_Systems.nixosModules.Sovran_SystemsOS ];
+ 			modules = [ 
+
+ 				./hardware-configuration.nix
+
+ 				Sovran_Systems.nixosModules.Sovran_SystemsOS 
+
+ 			];
 		
 		};
 	
