943e97045470cccc384d64e5a5d8e02d58fdf923
naturallaw77 <advbuwxeew@use.startmail.com>
2024-01-26 21:15:31 -0800

    Nixpkgs Update; Added 16GB SWAP; Added systmed bitcoin fix

--- for_new_sovran_pros/psp.sh
+++ for_new_sovran_pros/psp.sh
@@ -8,20 +8,27 @@ ENDCOLOR="\e[0m"
 
 lsblk
 
-echo -e "${GREEN}What block for Root drive (usually sda)?${ENDCOLOR}";read commitroot
+echo -e "${GREEN}What block for file-tree-root of drive (usually nvme0n1)?${ENDCOLOR}";read commitroot
 
 parted /dev/"$commitroot" -- mklabel gpt
-parted /dev/"$commitroot" -- mkpart primary 512MB -7MB
+parted /dev/"$commitroot" -- mkpart primary 512MB -16GB
+parted /dev/"$commitroot" -- mkpart swap linux-swap -16GB 100%
 parted /dev/"$commitroot" -- mkpart ESP fat32 1MB 512MB
-parted /dev/"$commitroot" -- set 2 esp on
+parted /dev/"$commitroot" -- set 3 esp on
 
 lsblk
 
-echo -e "${GREEN}What partition for Root drive (usually sda1)?${ENDCOLOR}";read commitrootpartition
+echo -e "${GREEN}What partition for Boot-Partition (usually nvme0n1p1)?${ENDCOLOR}";read commitbootpartition
 
-echo -e "${GREEN}What partition for Boot drive (usually sda2)?${ENDCOLOR}";read commitbootpartition
+echo -e "${GREEN}What partition for Main-Partition (usually nvme0n1p2)?${ENDCOLOR}";read commitmainpartition
 
-mkfs.ext4 -L nixos /dev/"$commitrootpartition"
+echo -e "${GREEN}What partition for Swap-Partition (usually nvme0n1p3)?${ENDCOLOR}";read commitswappartition
+
+
+
+mkfs.ext4 -L nixos /dev/"$commitmainpartition"
+
+mkswap -L swap /dev/"$commitswappartition"
 
 mkfs.fat -F 32 -n boot /dev/"$commitbootpartition"
 
@@ -30,6 +37,8 @@ mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot/efi                   
 
 mount /dev/disk/by-label/boot /mnt/boot/efi
+
+
 
 nixos-generate-config --root /mnt
 
@@ -48,12 +57,7 @@ cat <<EOT >> /mnt/etc/nixos/configuration.nix
   boot.loader.efi.canTouchEfiVariables = true;
 	boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  nix = {
-  	package = pkgs.nixUnstable;
-		extraOptions = ''
-			experimental-features = nix-command flakes
-		'';
-  };
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 	users.users = {
 		free = {
