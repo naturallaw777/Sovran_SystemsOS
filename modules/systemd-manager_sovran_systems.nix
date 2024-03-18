bea404cb476c406017f5870aea748ea0ecb05d1e
naturallaw77 <advbuwxeew@use.startmail.com>
2024-03-18 13:36:12 -0700

    Nixpkgs Update

--- modules/systemd-manager_sovran_systems.nix
+++ modules/systemd-manager_sovran_systems.nix
@@ -1,41 +1,41 @@
 { lib, stdenv, fetchzip, buildPackages }:
 
-			stdenv.mkDerivation rec {
-				pname = "systemd-manager";
-				version = "16";
+stdenv.mkDerivation rec {
+	pname = "systemd-manager";
+	version = "16";
 
-				src = fetchzip {
-					url = "https://github.com/hardpixel/systemd-manager/releases/download/v${version}/systemd-manager-v${version}.zip";
-					hash = "sha256-mbo0kSLkFpT71f/E4AJ0rMFLVtsks7u9snc8maMib4U=";
-					stripRoot = false;
-				};
+	src = fetchzip {
+		url = "https://github.com/hardpixel/systemd-manager/releases/download/v${version}/systemd-manager-v${version}.zip";
+		hash = "sha256-mbo0kSLkFpT71f/E4AJ0rMFLVtsks7u9snc8maMib4U=";
+		stripRoot = false;
+	};
 
-				passthru = {
-					extensionUuid = "systemd-manager@hardpixel.eu";
-					extensionPortalSlug = "systemd-manager";
-				};
+	passthru = {
+		extensionUuid = "systemd-manager@hardpixel.eu";
+		extensionPortalSlug = "systemd-manager";
+	};
 
-				nativeBuildInputs = [ buildPackages.glib ];
+	nativeBuildInputs = [ buildPackages.glib ];
 
-				buildPhase = ''
-					runHook preBuild
-					if [ -d schemas ]; then
-						glib-compile-schemas --strict schemas
-					fi
-					runHook postBuild
-				'';
+	buildPhase = ''
+		runHook preBuild
+		if [ -d schemas ]; then
+			glib-compile-schemas --strict schemas
+		fi
+		runHook postBuild
+	'';
 
-				installPhase = ''
-					runHook preInstall
-					mkdir -p $out/share/gnome-shell/extensions
-					 cp -r -T . $out/share/gnome-shell/extensions/${passthru.extensionUuid}
-					runHook postInstall
-				'';
+	installPhase = ''
+		runHook preInstall
+		mkdir -p $out/share/gnome-shell/extensions
+		 cp -r -T . $out/share/gnome-shell/extensions/${passthru.extensionUuid}
+		runHook postInstall
+	'';
 
-				meta = with lib; {
-					description = "GNOME Shell extension to manage systemd services";
-					license = licenses.gpl2Plus;
-					maintainers = with maintainers; [ ];
-					homepage = "https://github.com/hardpixel/systemd-manager";
-				};
-			}
+	meta = with lib; {
+		description = "GNOME Shell extension to manage systemd services";
+		license = licenses.gpl2Plus;
+		maintainers = with maintainers; [ ];
+		homepage = "https://github.com/hardpixel/systemd-manager";
+	};
+}
