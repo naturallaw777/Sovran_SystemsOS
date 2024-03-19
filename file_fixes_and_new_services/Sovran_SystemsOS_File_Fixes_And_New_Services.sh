dabafa61874476dc89c7c87ebee15a6566757aca
naturallaw77 <advbuwxeew@use.startmail.com>
2024-03-18 22:36:43 -0700

    Added Sovran_SystemsOS_External_Backup app

--- file_fixes_and_new_services/Sovran_SystemsOS_File_Fixes_And_New_Services.sh
+++ file_fixes_and_new_services/Sovran_SystemsOS_File_Fixes_And_New_Services.sh
@@ -40,6 +40,14 @@ rm -rf /home/free/Downloads/sovran-pro-flake-update2.sh
 
 rm -rf /home/free/Downloads/nextcloud_maintenance_window_fix.sh
 
+#### SCRIPT 5 ####
+
+/run/current-system/sw/bin/wget "https://git.sovransystems.com/Sovran_Systems/Sovran_SystemsOS/raw/branch/main/file_fixes_and_new_services/add_external_backup_app.sh"
+
+/run/current-system/sw/bin/bash /home/free/Downloads/add_external_backup_app.sh
+
+rm -rf /home/free/Downloads/add_external_backup_app.sh
+
 
 #### REMOVAL OF MAIN SCRIPT ####
 
