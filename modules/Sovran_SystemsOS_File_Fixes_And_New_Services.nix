b273e8584465f00a7a45d213aab337198c23f99f
naturallaw77 <advbuwxeew@use.startmail.com>
2024-08-09 15:58:54 -0700

    Udpated Sovran_Systems Helper

--- modules/Sovran_SystemsOS_File_Fixes_And_New_Services.nix
+++ modules/Sovran_SystemsOS_File_Fixes_And_New_Services.nix
@@ -3,21 +3,20 @@
 {
 
 	systemd.services.Sovran_SystemsOS_File_Fixes_And_New_Services = {
-	  
 
-	  unitConfig = {
-	    After = "btcpayserver.service";
-	    Requires = "network-online.target";
-	  };
-	   
-	  serviceConfig = {
-	    ExecStart = "/run/current-system/sw/bin/wget https://git.sovransystems.com/Sovran_Systems/Sovran_SystemsOS/raw/branch/main/file_fixes_and_new_services/Sovran_SystemsOS_File_Fixes_And_New_Services.sh -O /home/free/Downloads/Sovran_SystemsOS_File_Fixes_And_New_Services.sh ; /run/current-system/sw/bin/bash /home/free/Downloads/Sovran_SystemsOS_File_Fixes_And_New_Services.sh";
-	    RemainAfterExit = "yes";
-	    User = "root";
-	    Type = "oneshot";
-	  };
+		unitConfig = {
+			After = "btcpayserver.service";
+			Requires = "network-online.target";
+		};
+		 
+		serviceConfig = {
+			ExecStart = "/run/current-system/sw/bin/wget https://git.sovransystems.com/Sovran_Systems/Sovran_SystemsOS/raw/branch/main/file_fixes_and_new_services/Sovran_SystemsOS_File_Fixes_And_New_Services.sh -O /home/free/Downloads/Sovran_SystemsOS_File_Fixes_And_New_Services.sh ; /run/current-system/sw/bin/bash /home/free/Downloads/Sovran_SystemsOS_File_Fixes_And_New_Services.sh";
+			RemainAfterExit = "yes";
+			User = "root";
+			Type = "oneshot";
+		};
 
-	  wantedBy = [ "multi-user.target" ];
+		wantedBy = [ "multi-user.target" ];
 	
 	};
 
