8697d6fd2fa57ee1cf9c111f29fc3cccaf2f3747
naturallaw77 <advbuwxeew@use.startmail.com>
2025-02-13 06:49:36 -0800

    updated Sovran_Systems_File_Fixes

--- modules/Sovran_SystemsOS_File_Fixes_And_New_Services.nix
+++ modules/Sovran_SystemsOS_File_Fixes_And_New_Services.nix
@@ -10,6 +10,7 @@
 		};
 		 
 		serviceConfig = {
+			ExecStartPre= "/bin/sleep 30"
 			ExecStart = "/run/current-system/sw/bin/wget https://git.sovransystems.com/Sovran_Systems/Sovran_SystemsOS/raw/branch/main/file_fixes_and_new_services/Sovran_SystemsOS_File_Fixes_And_New_Services.sh -O /home/free/Downloads/Sovran_SystemsOS_File_Fixes_And_New_Services.sh ; /run/current-system/sw/bin/bash /home/free/Downloads/Sovran_SystemsOS_File_Fixes_And_New_Services.sh";
 			RemainAfterExit = "yes";
 			User = "root";
