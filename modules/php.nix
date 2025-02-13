08b62580116c57d108d74a884619d6d7c68d40bc
naturallaw77 <advbuwxeew@use.startmail.com>
2025-02-13 06:38:45 -0800

    php 83

--- modules/php.nix
+++ modules/php.nix
@@ -3,7 +3,7 @@
 
 let
 
-	custom-php = pkgs.php82.buildEnv {
+	custom-php = pkgs.php83.buildEnv {
 		extensions = { enabled, all }: enabled ++ (with all; [ bz2 apcu redis imagick memcached ]);
 		extraConfig = ''
 			
