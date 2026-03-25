b7053607da73b1462f50b5110ab047a4dcb85a18
naturallaw77 <advbuwxeew@use.startmail.com>
2026-03-24 22:51:39 -0500

    updated bip110 module

--- modules/bip110.nix
+++ modules/bip110.nix
@@ -4,17 +4,13 @@ let
   cfg = config.sovran_systemsOS;
 in
 {
-  options.sovran_systemsOS = {
-    features.bip110 = lib.mkEnableOption "Enable BIP110 bitcoind";
-
-    bip110.package = lib.mkOption {
-      type = lib.types.nullOr lib.types.package;
-      default = null;
-      description = "Custom bitcoind package for BIP110";
-    };
-  };
-
   config = lib.mkIf (cfg.features.bip110 && cfg.bip110.package != null) {
+
     services.bitcoind.package = cfg.bip110.package;
+
+    # Optional: also expose it in system packages if desired
+    environment.systemPackages = [
+      cfg.bip110.package
+    ];
   };
 }
