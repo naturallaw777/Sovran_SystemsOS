24f3ce67b817bf308527d9602558650e5288aeb9
naturallaw77 <advbuwxeew@use.startmail.com>
2026-03-24 19:03:33 -0500

    fixed bip110

--- modules/bip110.nix
+++ modules/bip110.nix
@@ -1,7 +1,7 @@
-{ config, lib, pkgs, bip110, ... }:
+{ config, lib, pkgs, ... }:
 
 lib.mkIf config.sovran_systemsOS.features.bip110 {
 
-services.bitcoind.packages = lib.mkForce bip110.packages.x86_64-linux.bitcoind-knots-bip-110;
+  services.bitcoind.packages = lib.mkForce bip110.packages.x86_64-linux.bitcoind-knots-bip-110;
 
 }
