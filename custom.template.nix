1cad2a4e87b715a6418720a71b85459513ad612c
naturallaw77 <advbuwxeew@use.startmail.com>
2026-04-06 21:13:22 -0500

    updated custom-template

--- custom.template.nix
+++ custom.template.nix
@@ -7,7 +7,8 @@
   #                                                         #
   #  Services, features, and roles are managed by the       #
   #  Sovran Hub. Any changes you make through the Hub       #
-  #  will appear in the "Hub Managed" section below.        #
+  #  will appear in a Hub Managed section added             #
+  #  automatically below.                                   #
   #                                                         #
   #  If you want to add your own NixOS modules or           #
   #  configuration, place them here — outside of the        #
